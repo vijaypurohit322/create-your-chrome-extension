c00b1ca75ba50a95319b0b2fb05fca67b4cc49ba
vijaypuohit322 <vijaypurohit322@gmail.com>
2021-01-28 00:58:04 +0530

    update feature

--- content.js
+++ content.js
@@ -1,18 +1,40 @@
 window.onload = () => {
     const button = document.createElement('button');
     button.id = "darkModeButton";
+    var count = 0;
     button.textContent = "Do it Dark";
     document.querySelector('#end').prepend(button);
 
-    button.addEventListener('click',()=> enableDarkMode());
+    button.addEventListener('click',()=> {
+        if(count==0)
+        {
+            count = 1;
+            enableDarkMode();
+
+        }
+        else
+        {
+            count = 0;
+            disableDarkMode();
+        }
+    }
+    );
 }
 
 function enableDarkMode()
 {
     document.getElementsByTagName('ytd-app')[0].style
     .backgroundColor = "Black";       
-    document.getElementsByTagName('div')[0].style
-    .backgroundColor = "Black";       
+    document.querySelector('#guide-renderer').style.backgroundColor = "#ddd";
+    document.querySelector('#video-title').style.color = "White"
+    
 
 
+}
+function disableDarkMode()
+{
+    document.getElementsByTagName('ytd-app')[0].style
+    .backgroundColor = "white";       
+    document.querySelector('#guide-renderer').style.backgroundColor = "white";
+    document.querySelector('#video-title').style.color = "Black"
 }
